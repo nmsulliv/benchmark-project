b5bc8c655b1659d86d3eeff026cf9ff848099cf7
nicole <nicole.sullivan1@bison.howard.edu>
2020-02-03 11:18:49 -0500

    Added the member function declarations

--- benchmark.h
+++ benchmark.h
@@ -1,2 +1,10 @@
 #include <iostream>
 //This is going to be my class header file
+
+class Benchmark {
+public:
+	void integer_operation();
+	void floatingp_operation();
+	void memory();
+	void hard_drive();
+}
