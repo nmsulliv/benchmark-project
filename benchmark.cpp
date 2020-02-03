147aa69e9f5e3ce1b5ca7d34f645fab0cbb0f85c
nicole <nicole.sullivan1@bison.howard.edu>
2020-02-03 11:35:47 -0500

    created a for loop for the addition portion of the benchmak

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <chrono>	//used for time 
 #include "benchmark.h"
+using namespace std::chrono;
 
 void integer_operation() {
+	auto start = high_resolution_clock::now(); //This will start the clock before bench
+											   //benchmarks run
+	int answ;
+	for (long i = 0; i < 100000000000; i++) {
+		c = 2 + 2;
+	}
+								
 
 }
