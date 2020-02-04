a400ce463f15e88d2583823a16e8e3e3e06426a8
nicole <nicole.sullivan1@bison.howard.edu>
2020-02-04 11:28:42 -0500

    Testing all at once.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 int main() {
 	Benchmark test1;
-// 	test1.Benchmark::integer_operation();
-// 	test1.Benchmark::floatingp_operation();
-// 	test1.Benchmark::memory();
+	test1.Benchmark::integer_operation();
+	test1.Benchmark::floatingp_operation();
+	test1.Benchmark::memory();
 	test1.Benchmark::hard_drive();
 
 
