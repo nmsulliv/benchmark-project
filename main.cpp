432bd8246e1e6ed116588253a7c4e53f453cec47
nicole <nicole.sullivan1@bison.howard.edu>
2020-02-03 21:58:34 -0500

    Added line to test hard drive benchmark

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main() {
 	Benchmark test1;
 // 	test1.Benchmark::integer_operation();
 // 	test1.Benchmark::floatingp_operation();
-	test1.Benchmark::memory();
+// 	test1.Benchmark::memory();
+	test1.Benchmark::hard_drive();
+
 
 return 0;
 }
